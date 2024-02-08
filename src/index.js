a29ec2abc652455322127c3d53283138ea1594a8
Mohan Dubey <mohandubeyang@gmail.com>
2024-02-08 13:59:43 +0530

    latest

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Mixpanel from 'mixpanel-browser';
 import ReactGA from 'react-ga';
-ReactGA.initialize('G-VVQTY9582V');
+ReactGA.initialize('G-7B2F6MD2G8');
 // Initialize Mixpanel with your project token
 Mixpanel.init('5413b0aa3437d534b244ab237b708470');
 const root = ReactDOM.createRoot(document.getElementById("root"));
